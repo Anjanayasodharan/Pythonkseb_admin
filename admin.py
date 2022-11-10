9ec1c332e3d76f195befa10eb45c3f213011a929
anjanayasodharan89@gmail.com <anjanayasodharan89@gmail.com>
2022-11-10 10:33:41 +0530

    first commit

---
+++ admin.py
@@ -0,0 +1,34 @@
+while True:
+    print("select an option from the menu")
+    print("1 add consumer")
+    print("2 search consumer ")  
+    print("3 delete consumer")
+    print("4 update consumer")    
+    print("5 view all consumer")
+    print("6 generate bill")
+    print("7 view bill")
+    print("8 exit")
+    choice = int(input('enter an option:'))
+    if(choice==1):
+        print('add consumer selected')
+        break
+    elif(choice==2):
+        print('search consumer selected')
+        break
+    elif(choice==3):
+        print('delete consumer selected')
+        break
+    elif(choice==4):
+        print('update consumer selected')
+        break
+    elif(choice==5):
+        print('view all consumer selected')
+        break
+    elif(choice==6):
+        print('generate bill selected')
+        break
+    elif(choice==7):
+        print('view bill selected')
+        break
+    elif(choice==8):
+        break
